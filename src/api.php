1189b8ff16ce79d851eddeab4995214b9b1df4ea
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-02 10:32:59 +1000

    Update api.php

--- src/api.php
+++ src/api.php
@@ -8,6 +8,7 @@ class api {
   
   public function OnLoad(): void
     $this->getLogger()info("Sky API for PM5 is Now Loaded.");
+  
   }
 public function OnEnable(): void
   $this->getLogger()info("Sky API Enabled: Have a Great Day!");
