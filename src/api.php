e341162db48a00a19101c84714eef11f0d91d9ce
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-02 16:54:09 +1000

    Update api.php

--- src/api.php
+++ src/api.php
@@ -1,16 +1,16 @@
 <?php
-namespace skyss0fly\SkyAPI
+namespace skyss0fly\SkyAPI;
   
   use pocketmine\Server;
 
 
 class api {
   
-  public function OnLoad(): void
+  public function OnLoad(): void {
     $this->getLogger()info("Sky API for PM5 is Now Loaded.");
   
   }
-public function OnEnable(): void
+public function OnEnable(): void {
   $this->getLogger()info("Sky API Enabled: Have a Great Day!");
 }
 }
